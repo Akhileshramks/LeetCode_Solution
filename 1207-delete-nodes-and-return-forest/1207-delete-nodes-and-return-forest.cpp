273dd2f4cf6e5054b8e11eda9552c4b593a35bec
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-07-17 10:28:39 +0530

    Time: 18 ms (41.04%), Space: 27.2 MB (23.69%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.cpp
@@ -12,21 +12,24 @@
 class Solution {
 public:
     set<int> to_delete_set;
-    TreeNode* deleteNode(TreeNode* root,vector<TreeNode*>& res,bool is_root){
+    TreeNode* dfs(TreeNode* root,vector<TreeNode*>& res,bool is_root){
         if(root == NULL) return NULL;
+
         bool is_delete = to_delete_set.find(root->val) != to_delete_set.end();
+
         if(is_root && !is_delete) res.push_back(root);
-        root->left = deleteNode(root->left,res,is_delete);
-        root->right = deleteNode(root->right,res,is_delete);
+
+        root->left = dfs(root->left,res,is_delete);
+        root->right = dfs(root->right,res,is_delete);
+
         if(is_delete) return NULL;
         return root;
     }
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         vector<TreeNode*> res;
-        for(int i : to_delete){
-            to_delete_set.insert(i);
-        }
-        deleteNode(root,res,true);
+        to_delete_set.insert(to_delete.begin(),to_delete.end());
+        
+        dfs(root,res,true);
         return res;
     }
 };
