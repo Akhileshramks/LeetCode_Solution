9729896af7db4dcd7ec43a779a5f66832f4e977b
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-07-21 20:28:31 +0530

    Time: 58 ms (69.28%), Space: 8.2 MB (86.46%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -3,18 +3,21 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        //vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(text1[i-1]==text2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    curr[j] = 1+prev[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    curr[j] = max(prev[j],curr[j-1]);
                 }
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
     int longestPalindromeSubseq(string s) {
         string s2 = s;
