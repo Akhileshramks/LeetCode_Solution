c319789e705cce651dad37abf4693d8f988e39e4
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-07-21 12:41:23 +0530

    Time: 334 ms (25.77%), Space: 79.6 MB (7.4%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int findsumways(int ind,int target,vector<int>& nums){
+    int findsumways(int ind,int target,vector<int>& nums,unordered_map<string,int>& dp){
         if(ind == 0){
             if (target == 0 && nums[0] == 0) return 2;
             else if (target==nums[0] || target==-nums[0]) return 1;
             else return 0;
         } 
-        int takenegative = findsumways(ind-1,target+nums[ind],nums);
-        int takepositive = findsumways(ind-1,target-nums[ind],nums);
-        return takenegative+takepositive;
+        string key = to_string(ind)+","+to_string(target);
+        if(dp.find(key)!=dp.end()) return dp[key];
+        int takenegative = findsumways(ind-1,target+nums[ind],nums,dp);
+        int takepositive = findsumways(ind-1,target-nums[ind],nums,dp);
+        return dp[key] = takenegative+takepositive;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<string,int> dp;
         int n = nums.size();
-        return findsumways(n-1,target,nums);
+        return findsumways(n-1,target,nums,dp);
     }
 };
