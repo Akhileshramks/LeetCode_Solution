5c03245c1730526dc9a1073a11f8ba50659f7488
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-08-26 14:14:39 +0530

    Time: 72 ms (21.85%), Space: 16.5 MB (70.79%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -24,7 +24,30 @@ public:
     bool isMatch(string s, string p) {
         int m = s.size();
         int n = p.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return wildcardmatching(m-1,n-1,s,p,dp);
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+        //return wildcardmatching(m-1,n-1,s,p,dp);
+
+        //Tabulation 
+
+        dp[0][0] = true;
+        bool flag = true;
+        for(int j = 1;j<=n;j++){
+            if(p[j-1]!='*' && flag) flag = false;
+            dp[0][j] = flag;
+        }
+
+        for(int i =1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1]=='*'){
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1]  ;
+                }
+                else dp[i][j] = false;
+            }
+        }
+
+        return dp[m][n];
     }
 };
