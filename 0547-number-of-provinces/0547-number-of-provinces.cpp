681892f696d36dbe41ca60acc94e32ba414e5166
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-08-30 08:41:52 +0530

    Time: 25 ms (28.7%), Space: 18.3 MB (78.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,28 +1,52 @@
+class Disjoint{
+    public:
+        vector<int> rank,parent;
+        Disjoint(int n){
+            parent.resize(n+1);
+            rank.resize(n+1,0);
+            for(int i= 0;i<=n;i++){
+                parent[i] = i;
+            }
+        }
+
+        int findparent(int s){
+            if(parent[s] == s) return s;
+            return parent[s] = findparent(parent[s]);
+        }
+
+        void unionset(int x,int y){
+            int up_x = findparent(x);
+            int up_y = findparent(y);
+            if( up_x == up_y){
+                return;
+            }
+            else if(rank[up_x] > rank[up_y]){
+                parent[up_y] = up_x;
+            }
+            else if(rank[up_x] < rank[up_y]){
+                parent[up_x] = up_y;
+            }
+            else{
+                parent[up_x] = up_y;
+                rank[up_y]++;
+            }
+            return;
+        }
+};
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool> visited(n,false);
-        queue<int> q;
-        int provinces = 0;
-        for(int src = 0 ; src < n ; src++){
-            if(!visited[src]){
-                provinces++;
-                q.push(src);
-                visited[src] = true;
-                while(!q.empty()){
-                    int s = q.front();
-                    q.pop();
-                    for(int des = 0;des<n;des++){
-                        if(isConnected[s][des] && !visited[des]){
-                            q.push(des);
-                            visited[des] = true;
-                        }
-                    }
+        Disjoint dsu(n);
+        int provinces = n;
+        for(int i = 0;i < n;i++){
+            for(int j = 0;j<n;j++){
+                if(isConnected[i][j] && dsu.findparent(i) != dsu.findparent(j)){
+                    dsu.unionset(i,j);
+                    provinces -- ;
                 }
             }
         }
         return provinces;
     }
-
 };
