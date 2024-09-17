3ab37accdc0a7ade1b72bb1d739797139f7e53f5
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-09-17 21:24:44 +0530

    Time: 47 ms (86.49%), Space: 31 MB (78.1%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n = hand.size();
-        if(n % groupSize != 0 ) return false;
+        if(n % groupSize != 0) return false;
         unordered_map<int,int> count;
-        for(auto i : hand) count[i]++;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(auto i : count) pq.push(i.first);
-        while(!pq.empty()){
-            int start = pq.top();
-            for(int next = start;next<start+groupSize;next++){
-                if(count.find(next) == count.end()) return false;
-                count[next]--;
-                if(count[next] == 0){
-                    if(pq.top() != next) return false;
-                    pq.pop();
+        for(int i : hand) count[i]++;
+        for(int i : hand){
+            int start = i;
+            while(count[start-1]) start--;
+            while(start<=i){
+                while(count[start]){
+                    for(int next = start;next<start+groupSize;next++){
+                        if(count[next] == 0) return false;
+                        count[next]--;
+                    }
                 }
+                start++;
             }
         }
         return true;
