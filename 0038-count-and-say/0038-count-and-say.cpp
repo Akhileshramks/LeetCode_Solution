f63547feb547d374ba43b88a1767f15686fd4d21
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-09-22 17:31:46 +0530

    Time: 11 ms (15.22%), Space: 10.2 MB (74.23%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     string countAndSay(int n) {
         if(n == 1) return "1";
-        string s = countAndSay(n-1);
-        int m = s.size();
-        int k = 1;
-        int start = s[0]-'0';
-        string res = "";
-        for(int i = 1;i<m;i++){
-            if(s[i]-'0' != start){
-                res += (to_string(k) + to_string(start));
-                start = s[i]-'0';
-                k = 1;
+        string s = "1";
+        for(int t =0;t<n-1;t++){
+            int m = s.size();
+            int k = 1;
+            int start = s[0]-'0';
+            string res = "";
+            for(int i = 1;i<m;i++){
+                if(s[i]-'0' != start){
+                    res += (to_string(k) + to_string(start));
+                    start = s[i]-'0';
+                    k = 1;
+                }
+                else k++;
             }
-            else k++;
+            res += to_string(k) + to_string(start);
+            s = res;
         }
-        res += to_string(k) + to_string(start);
-        cout<< res<<" ";
-        return res;
+        return s;
     }
 };
