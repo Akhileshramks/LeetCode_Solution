849bf9c373310f83fb351959b38cdec633253148
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-09-22 12:16:57 +0530

    Time: 15 ms (100%), Space: 27.7 MB (100%) - LeetHub

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/3496-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isPossible(int mountainHeight, vector<int>& workerTimes,long long time){
+        int len_w = workerTimes.size();
+        for(int i = 0 ; i < len_w; i++ ){
+            if(mountainHeight > 0){
+                long long c = time * 2 / workerTimes[i];
+                int n = (-1 + sqrt(1 + 4 * c)) / 2;
+                mountainHeight -= n;
+            }
+        }
+        return mountainHeight <= 0;
+    }
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long low = 0;
+        int maxi = *max_element(workerTimes.begin(),workerTimes.end());
+        long long high = 1LL * mountainHeight*(mountainHeight+1)*maxi /2;
+        long long ans;
+        while(low <= high){
+            long long mid = low + (high - low)/2;
+            if(isPossible(mountainHeight,workerTimes,mid)){
+                high = mid - 1;
+                ans = mid;
+            } 
+            else low = mid + 1;
+        }
+        return ans;
+    }
+};
+
+
+
+/*
+workerTimes[1] + workerTimes[1] * 2 + workerTimes[1] * 3 = 12
+workerTimes[1] * (1 + 2 + ... n) <= 12
+3 * (1 + 2 + ... n) <= 12
+(1 + 2 + ... n) <= 4
+
+n * (n + 1) /2 <=4
+
+n^2 + n <= 8
+
+n^2 + n - 8 <= 0
+
+n = - b  sqrt(b^2 - 4*a*c) / 2*b
+
+-1 + sqrt(1 - 4 * c) / 2
+
+c = mid * 2 / workerTimes[i] ;
+*/
