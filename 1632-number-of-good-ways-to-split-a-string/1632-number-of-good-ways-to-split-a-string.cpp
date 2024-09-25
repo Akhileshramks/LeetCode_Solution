81bc72e448a3825aabeeb8e72af15005c1c408a8
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-09-25 18:36:31 +0530

    Time: 24 ms (64.6%), Space: 11.4 MB (34.2%) - LeetHub

--- 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.cpp
+++ 1632-number-of-good-ways-to-split-a-string/1632-number-of-good-ways-to-split-a-string.cpp
@@ -2,6 +2,30 @@ class Solution {
 public:
     int numSplits(string s) {
         int n = s.size();
+        unordered_map<int,int> start,end;
+        for(int i =0;i<26;i++){
+            start[i] = -1;
+            end[i] = -1;
+        }
+        vector<int> res;
+        for(int i =0;i<n;i++){
+            if(start[s[i] - 'a'] == -1){
+                start[s[i] - 'a'] = i;
+            }
+            end[s[i] - 'a'] = i;
+        }
+        for(auto i : start){
+            if(i.second != -1) res.push_back(i.second);
+        }
+        for(auto i : end){
+            if(i.second != -1) res.push_back(i.second);
+        }
+        sort(res.begin(),res.end());
+        int mid = res.size()/2;
+        return res[mid] - res[mid-1];
+    }
+    int numSplits1(string s) {
+        int n = s.size();
         unordered_set<int> leftSet,rightSet;
         vector<int> left(n,0),right(n,0);
         for(int i =0;i<n;i++){
