798c1af8e7485c825d58b915789a40cd6a205b8d
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-09-27 06:49:50 +0530

    Time: 151 ms (71.11%), Space: 40.1 MB (84.57%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -0,0 +1,34 @@
+class MyCalendarTwo {
+public:
+    vector<pair<int,int>> calender;
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        int count = 0;
+        vector<pair<int,int>> overlap;
+        pair<int,int> interval;
+        cout<<start<<" "<<end<<" ";
+        for(auto i : calender){
+            if(end > i.first && start < i.second){
+                interval = {max(start,i.first),min(end,i.second)};
+                cout<<interval.first<<" "<<interval.second<<" ";
+                for(auto j : overlap){
+                    if(!(interval.second <= j.first || interval.first >= j.second)) count++;
+                    if(count >= 1) return false;
+                }
+                overlap.push_back(interval);
+            }
+        }
+        calender.push_back({start,end});
+        cout<<endl;
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
