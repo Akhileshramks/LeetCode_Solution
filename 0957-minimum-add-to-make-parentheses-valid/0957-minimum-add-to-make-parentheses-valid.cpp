5cb13a3f4cae372eac4c64a50b54be420b30e46b
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-09 22:03:11 +0530

    Time: 3 ms (42.94%), Space: 7.7 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-    //     stack<char> st;
-    //     int count = 0;
-    //     for(auto i : s){
-    //         if(i == ')'){
-    //             if(!st.empty() && st.top() == '('){
-    //                 st.pop();
-    //             }
-    //             else{
-    //                 count++;
-    //             }
-    //         }\
-    //         else{
-    //             st.push(i);
-    //         }
-    //     }
-    //     count+=st.size();
-    //     return count;
-    // }
-        int count = 0;
         int ans = 0;
-        for(auto i : s){
+        int count = 0;
+        for(char &i : s){
             if(i == '(') count++;
-            else{
-                if(count>0) count--;
-                else ans++;
-            }
+            else if(count > 0) count--;
+            else ans++;
         }
-        return ans+count;
+        return ans + count;
     }
 };
