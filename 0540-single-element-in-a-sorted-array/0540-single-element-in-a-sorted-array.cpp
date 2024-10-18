38af4a1077504d1cabade13213f733078188f6e9
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-18 10:37:38 +0530

    Time: 0 ms (100%), Space: 25 MB (63%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,13 +5,28 @@ public:
         if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
-        int low = 1,high = n-2;
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
-            else if((mid%2 == 0 && nums[mid] == nums[mid-1]) || (mid%2 == 1 && nums[mid] == nums[mid+1])) high = mid-1;
-            else low = mid+1;
+        int low = 1;
+        int high = n - 2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
+            if((mid % 2 == 0 && nums[mid] != nums[mid+1]) || (mid % 2 == 1 && nums[mid] != nums[mid-1])){
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
         }
         return -1;
     }
 };
+/*
+
+1,1,2,3,3,4,4,8,8
+0 1 2 3 4 5 6 7 8
+
+3,3,7,7,10,11,11
+0 1 2 3  4  5  6
+*/
