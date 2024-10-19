45e65de7369310c6e27d1a7eea6c045c4e340fcf
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-19 22:14:37 +0530

    Time: 1 ms (98.79%), Space: 20.9 MB (45.03%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        int n = asteroids.size();
-        vector<int> res;
         stack<int> st;
-        bool flag;
-        for(int i =0;i<n;i++){
-            int check =  asteroids[i];
-            while(!st.empty() && check<0 && st.top()>0){
-                if(abs(check)>st.top()){
+        int n = asteroids.size();
+        for(int i = 0; i < n ;i++){
+            int stone = asteroids[i];
+            while(!st.empty() && stone < 0 && st.top() > 0){
+                if(st.top() > abs(stone) ) stone = 0;
+                else if(st.top() == abs(stone)){
+                    stone = 0;
                     st.pop();
                 }
-                else if(abs(check) == st.top()){
-                    st.pop();
-                    check = 0;
-                }
-                else{
-                    check =0;
-                }
+                else st.pop();
             }
-            if(check!=0){
-                st.push(check);
-            }
+            if(stone != 0) st.push(stone);
         }
-
+        vector<int> res;
         while(!st.empty()){
-            int top = st.top();
+            res.push_back(st.top());
             st.pop();
-            res.push_back(top);
         }
         reverse(res.begin(),res.end());
         return res;
