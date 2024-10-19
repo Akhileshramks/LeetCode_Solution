60b58aae9ba1d4170b4f3d90b590ab8576b7eca5
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-19 08:58:48 +0530

    Time: 0 ms (100%), Space: 94.8 MB (70.9%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,25 +3,36 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
-        if(n>m) findMedianSortedArrays(nums2,nums1);
-        int k = (n+m+1)/2;
-        int low = max(0,k-m);
-        int high = min(k,n);
-        while(low<=high){
-            int cut1 = (low+high)>>1;
-            int cut2 = k-cut1;
-            int l1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
-            int l2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
-            int r1 = cut1 == n ? INT_MAX : nums1[cut1];
-            int r2 = cut2 == m ? INT_MAX : nums2[cut2];
-            if(l1<=r2 && l2<=r1){
-                if((n+m)%2 == 0){
-                    return (double)((max(l1,l2)+min(r1,r2)))/2;
-                }
-                return max(l1,l2);
+        if(n > m) return findMedianSortedArrays(nums2,nums1);
+        double median;
+        int left = (n+m+1)/2;
+        int low = 0;
+        int high = n;
+        bool flag = (n + m) % 2 == 0 ? true : false;
+        while(low <= high){
+            int l1 = INT_MIN;
+            int l2 = INT_MIN;
+            int r1 = INT_MAX;
+            int r2 = INT_MAX;
+            int mid1 = low + (high - low)/2;
+            int mid2 = left - mid1;
+            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            if(mid1 < n) r1 = nums1[mid1];
+            if(mid2 < m) r2 = nums2[mid2];
+
+            if(l1 <= r2 && l2 <= r1){
+                if(flag) median = (min(r1,r2) + max(l1,l2)) / 2.0;
+                else median = max(l1,l2);
+                return median;
             }
-            else if(l1>r2) high = cut1-1;
-            else low = cut1+1;
+
+            else if(l1 > r2){
+                high = mid1 - 1;
+            }
+            else{
+                low = mid1 + 1;
+            }
         }
         return -1;
     }
