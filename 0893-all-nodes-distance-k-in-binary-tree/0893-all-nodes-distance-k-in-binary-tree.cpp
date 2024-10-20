a9d181c00607965d39e431a21179812bff6fa02b
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-20 10:18:04 +0530

    Time: 5 ms (48.32%), Space: 15.5 MB (49.03%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,49 +9,44 @@
  */
 class Solution {
 public:
-    void linkParent(TreeNode* root,unordered_map<TreeNode*,TreeNode*> &parent){
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* node = q.front();
-            q.pop();
-            if(node->left){
-                parent[node->left] = node;
-                q.push(node->left);
-            }
-            if(node->right){
-                parent[node->right] = node;
-                q.push(node->right);
-            }
+    void linkParent(TreeNode* root,unordered_map<TreeNode*,TreeNode*>& parent){
+        if(root == NULL) return;
+        if(root->left){
+            parent[root->left] = root;
+            linkParent(root->left,parent);
+        }
+        if(root->right){
+            parent[root->right] = root;
+            linkParent(root->right,parent);
         }
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<TreeNode*,TreeNode*> parent;
-        linkParent(root,parent);
-        unordered_map<TreeNode*,bool> visited;
         queue<TreeNode*> q;
+        int cnt = 0;
+        unordered_map<TreeNode*,TreeNode*> parent;
+        unordered_map<TreeNode*,bool> visited;
+        linkParent(root,parent);
+        vector<int> res;
         q.push(target);
         visited[target] = true;
-        int cnt = 0;
-        vector<int> res;
-        while(!q.empty() && cnt<k){
+        while(!q.empty() && cnt < k){
             int n = q.size();
             cnt++;
-            for(int i = 0;i<n;i++){
+            for(int i =0;i<n;i++){
                 TreeNode* node = q.front();
                 q.pop();
                 if(node->left && !visited[node->left]){
                     q.push(node->left);
                     visited[node->left] = true;
-                } 
-                if(node -> right && !visited[node -> right]){
+                }
+                if(node->right && !visited[node->right]){
                     q.push(node->right);
                     visited[node->right] = true;
                 }
                 if(parent[node] && !visited[parent[node]]){
                     q.push(parent[node]);
                     visited[parent[node]] = true;
-                } 
+                }
             }
         }
         while(!q.empty()){
