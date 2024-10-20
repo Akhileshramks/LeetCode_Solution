b7f578d747042ba1e9fa09ba2a8a9f8621525f8f
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-20 20:09:35 +0530

    Time: 0 ms (100%), Space: 25.1 MB (13.02%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -12,7 +12,7 @@ public:
             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
                 return nums[mid];
             }
-            if((mid % 2 == 0 && nums[mid] != nums[mid+1]) || (mid % 2 == 1 && nums[mid] != nums[mid-1])){
+            if((mid % 2 == 0 && nums[mid] == nums[mid-1]) || (mid % 2 == 1 && nums[mid] == nums[mid+1])){
                 high = mid-1;
             }
             else{
