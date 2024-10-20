2499fab1e658754e46c1def549bc0496b60c7fa6
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-20 13:53:26 +0530

    Time: 0 ms (100%), Space: 85.9 MB (96.2%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int count5 =0;
-        int count10 = 0;
-        for(int i  : bills){
+        int count5 = 0,count10  = 0,count20 = 0;
+        for(auto &i : bills){
             if(i == 5) count5++;
             else if(i == 10){
-                if(count5>0){
-                    count5--;
-                    count10++;
-                }
-                else{
-                    return false;
-                }  
+                count10++;
+                if(count5 > 0) count5--;
+                else return false;
             }
             else{
-                if(count10 > 0 && count5>0){
-                    count10--;
-                    count5--;
-                }
-                else if(count5>=3){
-                    count5-=3;
-                }
-                else{
-                    return false;
-                }
+                count20++;
+                if(count10 > 0 && count5 > 0) count5--,count10--;
+                else if(count5>=3) count5-=3;
+                else return false;
             }
         }
         return true;
