1f17c84684d484a4ad45bf3672f96e1e974be25a
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-20 15:57:49 +0530

    Time: 49 ms (99.69%), Space: 93.6 MB (48.59%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -5,12 +5,11 @@ public:
     }
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),comp);
-        int end = intervals[0][1];
-        int prev = 0,n = intervals.size(),count =1;
+        int end = intervals[0][1],n = intervals.size(),count =1;
         for(int i =1;i<n;i++){
-            if(intervals[i][0]>=intervals[prev][1]){
+            if(intervals[i][0]>=end){
                 count++;
-                prev = i;
+                end = intervals[i][1];
             }
         }
         return n-count;
