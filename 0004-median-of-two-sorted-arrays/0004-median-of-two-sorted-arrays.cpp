b5b32eb02b4931a15b2d926a0c6a8259d1d84741
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-28 17:43:54 +0530

    Time: 0 ms (100%), Space: 94.7 MB (71.08%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,36 +4,44 @@ public:
         int n = nums1.size();
         int m = nums2.size();
         if(n > m) return findMedianSortedArrays(nums2,nums1);
-        double median;
-        int left = (n+m+1)/2;
         int low = 0;
         int high = n;
-        bool flag = (n + m) % 2 == 0 ? true : false;
+        int left = (n + m + 1) / 2;
+        bool even = (n+m)%2 == 0;
+        double median;
         while(low <= high){
-            int l1 = INT_MIN;
-            int l2 = INT_MIN;
-            int r1 = INT_MAX;
-            int r2 = INT_MAX;
-            int mid1 = low + (high - low)/2;
+            int mid1 = low + (high-low)/2;
             int mid2 = left - mid1;
-            if(mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
-            if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            int l1 = INT_MIN,l2 = INT_MIN,r1 = INT_MAX,r2 = INT_MAX;
+            if(mid1-1 >= 0) l1 = nums1[mid1-1];
+            if(mid2-1 >= 0) l2 = nums2[mid2-1];
             if(mid1 < n) r1 = nums1[mid1];
             if(mid2 < m) r2 = nums2[mid2];
-
             if(l1 <= r2 && l2 <= r1){
-                if(flag) median = (min(r1,r2) + max(l1,l2)) / 2.0;
-                else median = max(l1,l2);
-                return median;
+                if(even){
+                    median = (max(l1,l2) + min(r1,r2))/2.0;
+                }
+                else{
+                    median = max(l1,l2);
+                }
+                break;
             }
-
             else if(l1 > r2){
-                high = mid1 - 1;
+                high = mid1-1;
             }
             else{
-                low = mid1 + 1;
+                low = mid1+1;
             }
         }
-        return -1;
+        return median;
     }
 };
+
+/*
+
+  1 | 4 5 
+2 3 | 6
+
+1 2         | INT_MAX 
+  INT_MIN   | 3 4
+*/
