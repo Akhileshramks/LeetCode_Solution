8609c2443a1a21d524a7997c0b99e393dc8dd51d
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-28 23:10:31 +0530

    Time: 9 ms (66.08%), Space: 12.2 MB (50.61%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<int,int> mpp;
         int n = s.size();
-        int l =0,r = 0;
-        int maxi =0;
-        while(r<n){
-            int c = s[r]-'a';
-            if(mpp.find(c)!=mpp.end()){
-                l = max(l,mpp[c]+1);
+        int l = 0;
+        unordered_map<int,int> mpp;
+        int maxi = 0;
+        for(int r = 0;r < n;r++){
+            if(mpp.find(s[r]-'a') != mpp.end()){
+                l = max(l,mpp[s[r]-'a'] + 1);
             }
-            mpp[c] = r;
+            mpp[s[r]-'a'] = r;
             maxi = max(maxi,r-l+1);
-            r++;
         }
         return maxi;
-
-        // int n = s.size();
-        // int maxi =0;
-        // unordered_set<int> st;
-        // int l=0;
-        // for(int r=0;r<n;r++){
-        //     if(st.find(s[r]-'a')!=st.end()){
-        //         while(l<r && st.find(s[r]-'a')!=st.end()){
-        //             st.erase(s[l]-'a');
-        //             l++;
-        //         }
-        //     }
-        //     st.insert(s[r]-'a');
-        //     maxi = max(maxi,r-l+1);
-        // }
-        // return maxi;
-
-
     }
 };
