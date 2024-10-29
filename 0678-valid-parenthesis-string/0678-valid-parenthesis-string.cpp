a7b75eb99117ce7e68e15edd4bc5492f531ba0d5
Akhilesh Ram K S <akhileshram710@gmail.com>
2024-10-29 08:10:04 +0530

    Time: 3 ms (9.04%), Space: 7.5 MB (85.75%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int mini = 0,maxi = 0;
-        for(auto &i : s){
-            if(i == '(') mini++, maxi++;
-            else if(i == ')') mini-- , maxi--;
-            else  mini -= 1, maxi += 1;
-            if(mini < 0) mini = 0;
-            if(maxi < 0) return false;
+        int l = 0,star = 0;
+        for(char i : s){
+            if(i == '(') l++;
+            else if(i == '*') star++;
+            else if(i == ')'){
+                if(l>0) l--;
+                else if(star>0) star--;
+                else return false;
+            }
         }
-        return mini == 0;
+        reverse(s.begin(),s.end());
+        l=0,star=0;
+        for(char i : s){
+            if(i == ')') l++;
+            else if(i == '*') star++;
+            else if(i == '('){
+                if(l>0) l--;
+                else if(star>0) star--;
+                else return false;
+            }
+        }
+        return true;
     }
 };
