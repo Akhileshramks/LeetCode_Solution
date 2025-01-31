50b17f9933892d0e462f34f77f0c86557c8f2e98
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-01-31 20:57:54 +0530

    Time: 288 ms (40.73%), Space: 159.5 MB (37.13%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.cpp
+++ 0854-making-a-large-island/0854-making-a-large-island.cpp
@@ -1,12 +1,12 @@
-class Disjoint{
+class DisjointSet{
     public:
         vector<int> parent;
         vector<int> size;
 
-        Disjoint(int n){
-            parent.resize(n+1);
-            size.resize(n+1,1);
-            for(int i=0;i<=n;i++){
+        DisjointSet(int n){
+            parent.resize(n);
+            size.resize(n,1);
+            for(int i = 0;i < n;i++){
                 parent[i] = i;
             }
         }
@@ -16,65 +16,63 @@ class Disjoint{
             return parent[u] = find(parent[u]);
         }
 
-        void unionfind(int u,int v){
-            int up_u = find(u);
-            int up_v = find(v);
-            if(up_u == up_v) return;
-            else if(size[up_u] > size[up_v]){
-                parent[up_v] = up_u;
-                size[up_u]+=size[up_v];
+        void unionSet(int u,int v){
+            int parent_u = find(u);
+            int parent_v = find(v);
+            if(parent_u == parent_v) return;
+            else if(size[parent_u] > size[parent_v]){
+                parent[parent_v] = parent_u;
+                size[parent_u] += size[parent_v];
             }
             else{
-                parent[up_u] = up_v;
-                size[up_v]+=size[up_u];
+                parent[parent_u] = parent_v;
+                size[parent_v] += size[parent_u];
             }
-            return;
         }
+
 };
 class Solution {
 public:
     int largestIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        vector<pair<int,int>> coordinates = {{0,1},{1,0},{-1,0},{0,-1}};
-        Disjoint graph(n*m);
-        int maxi =0;
-        for(int i = 0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(grid[i][j]){
-                    for(auto [dx,dy] : coordinates){
-                        int xx = i+dx;
-                        int yy = j+dy;
-                        if(xx>=0 && xx<n && yy>=0 && yy<m && grid[xx][yy]){
-                            graph.unionfind(n*i+j,n*xx+yy);
+        int m = grid.size();
+        int n = grid[0].size();
+        int total = m*n;
+        DisjointSet graph(total);
+        int x[] = {0,1,0,-1};
+        int y[] = {1,0,-1,0};
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
+                if(grid[i][j] == 1){
+                    for(int k = 0;k < 4;k++){
+                        int dx = i + x[k];
+                        int dy = j + y[k];
+                        if(dx >= 0 && dx < m && dy >=0 && dy < n && grid[dx][dy]){
+                            graph.unionSet(dx*n+dy,i*n+j);
                         }
                     }
+
                 }
             }
         }
-        for(int i = 0;i<n;i++){
-            for(int j =0;j<m;j++){
+        int maxi = 0;
+        for(int i = 0;i < total;i++) maxi = max(maxi,graph.size[i]);
+        for(int i =0;i < m;i++){
+            for(int j = 0;j < n;j++){
                 if(grid[i][j] == 0){
                     unordered_set<int> st;
-                    int island = 1;
-                    for(auto [dx,dy] : coordinates){
-                        int xx = i+dx;
-                        int yy = j+dy;
-                        if(xx>=0 && xx<n && yy>=0 && yy<m && grid[xx][yy]){
-                            int up_u = graph.find(n*xx+yy);
-                            st.insert(up_u);
+                    for(int k = 0;k < 4;k++){
+                        int dx = i + x[k];
+                        int dy = j + y[k];
+                        if(dx >= 0 && dx < m && dy >=0 && dy <n && grid[dx][dy]){
+                            st.insert(graph.find(dx*m+dy));
                         }
                     }
-                    for(int i : st){
-                        island+=graph.size[i];
-                    }
-                    maxi = max(maxi,island);
+                    int res = 1;
+                    for(int p : st) res += graph.size[p];
+                    maxi = max(maxi,res);
                 }
-
             }
         }
-        if(maxi == 0) return *max_element(graph.size.begin(),graph.size.end());
         return maxi;
     }
 };
