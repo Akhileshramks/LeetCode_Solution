6dad33a9dbc819b5688c1c475fb36689c4b813e9
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-03-14 07:05:10 +0530

    Time: 51 ms (14.61%), Space: 88.2 MB (41.19%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPossible(vector<int>& candies,long long k,int target,int n){
+        for(int i = n-1;i >= 0 && k > 0;i--){
+            int children = candies[i]/target;
+            k -= children;
+        }
+        return k <= 0;
+    }
+    int maximumCandies(vector<int>& candies, long long k) {
+        int n = candies.size();
+        sort( candies.begin(),candies.end());
+        int low = 1,high = candies[n-1];
+        int ans = 0;
+        //if(n == 1 &&) return candies[0];
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(mid != 0 && isPossible(candies,k,mid,n)){
+                ans = mid;
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
