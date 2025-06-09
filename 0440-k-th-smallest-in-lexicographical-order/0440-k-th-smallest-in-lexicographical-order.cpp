7575088c2f8bd1e81e1a730e5f30201623cabd52
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-06-10 00:23:36 +0530

    Time: 0 ms (100%), Space: 7.7 MB (83.39%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int findStep(int n, long long curr, long long next){
-        int step = 0;
-        while(curr <= n){
-            step += min((long long)n+1,next) - curr;
-            curr = curr * 10;
-            next = next * 10;
-        }
-        return step;
-    }
     int findKthNumber(int n, int k) {
-        int i = 1; 
-        while(i <= n){
-            int step = findStep(n,i,i+1);
-            if(step >= k){
-                i = i*10;
+        int currNum = 1;
+        k--;
+        while(k > 0){
+            int step = countNoOfChildren(n, currNum, currNum + 1);
+            if(step > k){
+                currNum *= 10;
                 k--;
             }
             else{
+                currNum++;
                 k -= step;
-                i++;
             }
-            if(k == 1) return i;
         }
-        return 1;
+        return currNum;
+    }
+    int countNoOfChildren(long n, long leftSibling, long rightSibling){
+        long count = 0;
+        while(leftSibling <= n){
+            long right = min((long)n+1, rightSibling);
+            long left = leftSibling;
+            count += right - left;
+            leftSibling *= 10;
+            rightSibling *= 10;
+        }
+        return count;
     }
 };
