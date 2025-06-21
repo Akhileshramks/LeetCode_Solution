cea608da3bdcd9044ed739fe9b4a2e5b7ff7babd
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-06-21 19:36:43 +0530

    Time: 11 ms (48.91%), Space: 51 MB (5.07%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int assumeGivenCharMin(string word, int ind, vector<int>& freq, int k){
+        int x = freq[ind];
+        int del = 0;
+        for(int i = 0;i < 26;i++){
+            int y = freq[i];
+            if(y < x) del += y;
+            else if(y - x < k) continue;
+            else del += y - x - k;
+        }
+        return del;
+    }
+    int minimumDeletions(string word, int k) {
+        vector<int> freq(26);
+        for(int i = 0;i < word.size(); i++){
+            freq[word[i] - 'a']++;
+        }
+        int ans =  word.size();
+        for(int i = 0;i < 26;i++){
+            if(freq[i] > 0){
+                ans = min(ans, assumeGivenCharMin(word,i,freq,k));
+            }
+        }
+        return ans;
+
+    }
+};
+
+
+/*
+
+Case 1 : 
+y < x => delete all y
+
+Case 2 :
+y > x =>
+        1. y - x < k => no issue
+        2. y - x > k => y - x - k characters need to be deleted
+
+Case 3 :
+ y == x no issues
+*/
+
