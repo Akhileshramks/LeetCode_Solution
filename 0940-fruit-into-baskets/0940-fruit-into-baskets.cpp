ad200952773f24bafebe69d521be7ce962e3b134
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-06-29 17:13:42 +0530

    Time: 31 ms (82.95%), Space: 74.8 MB (88.86%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
-        int left = 0;
-        int sum = 0;
-        unordered_map<int,int> select;
-        for(int right = 0;right<n;right++){
-            select[fruits[right]]++;
-            while(select.size()>2){
-                select[fruits[left]]--;
-                if(select[fruits[left]]==0){
-                    select.erase(fruits[left]);
+        int k = 2;
+        unordered_map<int,int> lastSeen;
+        int ans = 0;
+        int left = 0,right = 0,n = fruits.size();
+        for(right = 0;right < n;right++){
+            lastSeen[fruits[right]] = right;
+            if(lastSeen.size() > k && left <= right){
+                int minIdx = INT_MAX;
+                int fruitToRemove = -1;
+                for(auto [fruit,idx] : lastSeen){
+                    if(idx < minIdx){
+                        minIdx = idx;
+                        fruitToRemove = fruit;
+                    }
                 }
-                left++;
+                left = minIdx + 1;
+                lastSeen.erase(fruitToRemove);
             }
-            sum = max(sum,right-left+1);
+            ans = max(ans, right - left + 1);
         }
-        return sum;
+        return ans;
     }
 };
