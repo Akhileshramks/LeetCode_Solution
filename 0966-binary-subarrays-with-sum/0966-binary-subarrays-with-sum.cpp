587c1ac9d496ecdcf026a3dbd4928330d75645df
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-06-30 21:27:55 +0530

    Time: 0 ms (100%), Space: 32.6 MB (60%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
-    int numSubarrayWithSumAtleastGoal(vector<int>& nums, int goal){
-        int l = 0,r = 0,n = nums.size();
-        int count = 0;
-        int sum = 0;
-        for(r = 0;r < n;r++){
-            sum += nums[r];
-            while(sum > goal && l <= r){
-                sum -= nums[l];
+    // int numSubarrayWithSumAtleastGoal(vector<int>& nums, int goal){
+    //     int l = 0,r = 0,n = nums.size();
+    //     int count = 0;
+    //     int sum = 0;
+    //     for(r = 0;r < n;r++){
+    //         sum += nums[r];
+    //         while(sum > goal && l <= r){
+    //             sum -= nums[l];
+    //             l++;
+    //         }
+    //         count += (r - l + 1);
+    //     }
+    //     return count;
+    // }
+    // int numSubarraysWithSum(vector<int>& nums, int goal) {
+    //     return numSubarrayWithSumAtleastGoal(nums, goal) - numSubarrayWithSumAtleastGoal(nums, goal - 1);
+    // }
+    int numSubarraysWithSum(vector<int>& nums, int goal){
+        int n = nums.size();
+        int currSum = 0;
+        int zeroCount = 0;
+        int l = 0;
+        int res = 0;
+        for(int r = 0;r < n;r++){
+            currSum += nums[r];
+            while(l < r && (nums[l] == 0 || currSum > goal)){
+                if(nums[l] == 0) zeroCount++;
+                else zeroCount = 0;
+
+                currSum -= nums[l];
                 l++;
             }
-            count += (r - l + 1);
+            if(currSum == goal) res += 1 + zeroCount;
         }
-        return count;
+        return res;
     }
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return numSubarrayWithSumAtleastGoal(nums, goal) - numSubarrayWithSumAtleastGoal(nums, goal - 1);
-    }
+
 };
