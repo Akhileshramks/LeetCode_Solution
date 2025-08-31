abac29b631de0a0ca49ea441d6cdd468aa7176a4
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-08-31 10:34:22 +0530

    Time: 0 ms (100%), Space: 20.5 MB (40.07%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,52 +1,33 @@
-class Disjoint{
-    public:
-        vector<int> rank,parent;
-        Disjoint(int n){
-            parent.resize(n+1);
-            rank.resize(n+1,0);
-            for(int i= 0;i<=n;i++){
-                parent[i] = i;
-            }
-        }
-
-        int findparent(int s){
-            if(parent[s] == s) return s;
-            return parent[s] = findparent(parent[s]);
-        }
-
-        void unionset(int x,int y){
-            int up_x = findparent(x);
-            int up_y = findparent(y);
-            if( up_x == up_y){
-                return;
-            }
-            else if(rank[up_x] > rank[up_y]){
-                parent[up_y] = up_x;
-            }
-            else if(rank[up_x] < rank[up_y]){
-                parent[up_x] = up_y;
-            }
-            else{
-                parent[up_x] = up_y;
-                rank[up_y]++;
-            }
-            return;
-        }
-};
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        Disjoint dsu(n);
-        int provinces = n;
+        vector<vector<int>> adj(n);
         for(int i = 0;i < n;i++){
-            for(int j = 0;j<n;j++){
-                if(isConnected[i][j] && dsu.findparent(i) != dsu.findparent(j)){
-                    dsu.unionset(i,j);
-                    provinces -- ;
+            for(int j = 0;j < n;j++){
+                if(isConnected[i][j] == 1) adj[i].push_back(j);
+            }
+        }
+        vector<bool> visited(n, false);
+        queue<int> q;
+        int count = 0;
+        for(int i = 0;i < n;i++){
+            if(!visited[i]){
+                count += 1;
+                visited[i] = true;
+                q.push(i);
+                while(!q.empty()){
+                    int front = q.front();
+                    q.pop();
+                    for(int m : adj[front]){
+                        if(!visited[m]){
+                            q.push(m);
+                            visited[m] = true;
+                        }
+                    }
                 }
             }
         }
-        return provinces;
+        return count;
     }
 };
