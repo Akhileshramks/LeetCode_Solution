5d0b1564eedc1a9e47060dc570420b9aa323165b
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-08-31 14:35:57 +0530

    Time: 0 ms (100%), Space: 18.2 MB (60.03%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         queue<pair<int,int>> q;
-        int n = image.size();
-        int m = image[0].size();
-
-        int check = image[sr][sc];
-        if(check == color) return image;
-        q.push({sr,sc});
+        int m = image.size(), n = image[0].size();
+        int srcColor = image[sr][sc];
+        if(srcColor == color) return image;
         image[sr][sc] = color;
-       
+        q.push({sr, sc});
+        int dx[4] = {0,0,1,-1};
+        int dy[4] = {-1,1,0,0};
         while(!q.empty()){
-            auto p = q.front();
+            auto top = q.front();
+            int x = top.first;
+            int y = top.second;
             q.pop();
-            vector<pair<int,int>> coordinates = {{0,1},{1,0},{0,-1},{-1,0}};
-            for(auto i : coordinates){
-                int xx = i.first + p.first;
-                int yy = i.second + p.second;
-                if(xx>=0 && xx<n && yy>=0 && yy<m && image[xx][yy] == check){
-                    q.push({xx,yy});
-                    image[xx][yy] = color;
+            image[x][y] = color;
+            for(int k = 0;k < 4;k++){
+                int dxx = x + dx[k];
+                int dyy = y + dy[k];
+                if(dxx >= 0 && dyy >= 0 && dxx < m && dyy < n && image[dxx][dyy] == srcColor){
+                    image[dxx][dyy] = color;
+                    q.push({dxx,dyy});
                 }
             }
         }
-
         return image;
     }
 };
