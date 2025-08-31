eff8159f559f94a620f5dff7b57083b9e033d6c2
Akhilesh Ram K S <akhileshram710@gmail.com>
2025-08-31 14:06:35 +0530

    Time: 0 ms (100%), Space: 16.6 MB (88.52%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int time = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        int freshCount = 0;
         queue<pair<int,int>> q;
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i = 0;i < n;i++){
-            for(int j =0;j<m;j++){
+        for(int i = 0;i < m;i++){
+            for(int j = 0;j < n;j++){
                 if(grid[i][j] == 2) q.push({i,j});
+                else if(grid[i][j] == 1) freshCount++;
             }
         }
-        int count = 0;
-        vector<pair<int,int>> coordinates = {{0,-1},{-1,0},{1,0},{0,1}};
+        int dx[4] = {0,0,-1,1};
+        int dy[4] = {-1,1,0,0};
         while(!q.empty()){
-            int noOfNodes = q.size();
-            count++;
-            for(int i =0;i < noOfNodes;i++){
-                pair<int,int> qFront = q.front();
-                int x = qFront.first;
-                int y = qFront.second;
+            int len = q.size();
+            for(int i = 0;i < len;i++){
+                auto top = q.front();
                 q.pop();
-                for(auto c : coordinates){
-                    int dx = c.first;
-                    int dy = c.second;
-                    int xx = x + dx;
-                    int yy = y + dy;
-                    if(xx >= 0 && xx < n && yy >= 0 && yy < m && grid[xx][yy] == 1){
-                        grid[xx][yy] = 2;
-                        q.push({xx,yy});
+                int x = top.first;
+                int y = top.second;
+                for(int k = 0;k < 4;k++){
+                    int dxx = x + dx[k];
+                    int dyy = y + dy[k];
+                    if(dxx >= 0 && dyy >= 0 && dxx < m && dyy < n && grid[dxx][dyy] == 1){
+                        freshCount--;
+                        q.push({dxx,dyy});
+                        grid[dxx][dyy] = 2;
                     }
                 }
             }
+            if(!q.empty()) time++;
         }
-        for(int i = 0; i < n;i++){
-            for(int j = 0;j < m;j++){
-                if(grid[i][j] == 1) return -1;
-            }
-        }
-        return count > 0 ? count-1 : 0;
+
+        return freshCount == 0 ? time : -1;
     }
 };
